6916001419f8291b1ba2d0ebe3489f83f8c496aa
RomanIgambaev <roman.igambaev11@gmail.com>
2023-08-16 14:59:48 +0500

    Skier

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double firstDayKm = 10;
+        int countDay = 1;
+        double sumKm = 0;
+        while (countDay <= 9) {
+            firstDayKm = firstDayKm + firstDayKm/100*10;
+            System.out.println("Пробег лыжиника на каждый день " + firstDayKm);
+            sumKm += firstDayKm;
+            countDay++;
+        }
+        System.out.println("В сумме лыжник пробежал за 10 дней:" + (int)sumKm );
     }
 }
